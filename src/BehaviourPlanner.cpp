ef2fba33d1fd5b064f1f56639b7aadd424400d47
Ivan Danov <idanov@users.noreply.github.com>
2017-08-02 23:00:49 +0100

    Simplify behaviour planner

--- src/BehaviourPlanner.cpp
+++ src/BehaviourPlanner.cpp
@@ -5,36 +5,14 @@ BehaviourPlanner::BehaviourPlanner(string initial_state) {
 }
 
 Car BehaviourPlanner::updatePlan(Car ego, const vector<vector<Car>>& predictions) {
-  int lane = ego.getLane();
-  vector<Car> leader_path;
-  double leader_dist = 100000;
-  for(vector<Car> v_path : predictions) {
-    if(v_path[0].getLane() == lane && v_path[0].s > ego.s) {
-      if(v_path[0].s - ego.s < leader_dist) {
-        leader_path = v_path;
-        leader_dist = leader_path[0].s - ego.s;
-      }
-    }
-  }
+  Car goal(ego);
 
-  if(!leader_path.empty()) {
-    double t = dt * leader_path.size();
-    Car leaderEnd = *leader_path.end();
-    ego.s = min(leaderEnd.s - car_buffer, ego.s + ego.s_dot * t);
-    ego.s_dot = min(max_speed, leaderEnd.s_dot);
-    ego.s_ddot = 0;
-    ego.d = ego.getLane() * LANE_WIDTH + 2;
-    ego.d_dot = 0;
-    ego.d_ddot = 0;
-  } else {
-    double t = dt * n_steps;
-    ego.s = ego.s + ego.s_dot * t;
-    ego.s_dot = max_speed;
-    ego.s_ddot = 0;
-    ego.d = ego.getLane() * LANE_WIDTH + 2;
-    ego.d_dot = 0;
-    ego.d_ddot = 0;
-  }
+  goal.s += max_speed;
+  goal.s_dot = max_speed;
+  goal.s_ddot = 0;
+  goal.d = 6;
+  goal.d_dot = 0;
+  goal.d_ddot = 0;
 
-  return ego;
+  return goal;
 }
